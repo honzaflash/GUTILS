973bc02acf4cfdf98d826f1099083b18edcd64f4
Honza Rychly <honza@axiomdatascience.com>
2025-04-23 16:44:19 -0800

    Make `test_profiles` use deployment config
    - use the provided `filter_args` in the tests
    - add the deployment.json for glider 191

--- gutils/tests/test_profiles.py
+++ gutils/tests/test_profiles.py
@@ -1,5 +1,7 @@
 #!python
 # coding=utf-8
+import json
+from pathlib import Path
 import pandas as pd
 import pytest
 import logging
@@ -37,11 +39,11 @@ unit_191_test_files = [
 bass_test_files = [
     { 'name': 'usf_bass_2016_252_1_0_sbd.dat', 'expected': { 'count': 6 } },
     { 'name': 'usf_bass_2016_252_1_6_sbd.dat', 'expected': { 'count': None } },
-    { 'name': 'usf_bass_2016_252_1_8_sbd.dat', 'expected': { 'count': 8 } },
+    { 'name': 'usf_bass_2016_252_1_8_sbd.dat', 'expected': { 'count': 6 } },
     { 'name': 'usf_bass_2016_252_1_11_sbd.dat', 'expected': { 'count': None } },
-    { 'name': 'usf_bass_2016_252_1_17_sbd.dat', 'expected': { 'count': 7, 'points': { 0: 18, 1: 140, 6: 177 } } },
-    { 'name': 'usf_bass_2016_253_0_1_sbd.dat', 'expected': { 'count': 13 } },
-    { 'name': 'usf_bass_2016_253_0_4_sbd.dat', 'expected': { 'count': 29 } },
+    { 'name': 'usf_bass_2016_252_1_17_sbd.dat', 'expected': { 'count': 8, 'points': { 0: 1, 1: 11, 6: 134 } } },
+    { 'name': 'usf_bass_2016_253_0_1_sbd.dat', 'expected': { 'count': 14 } },
+    { 'name': 'usf_bass_2016_253_0_4_sbd.dat', 'expected': { 'count': 28 } },
     { 'name': 'usf_bass_2016_253_0_6_sbd.dat', 'expected': { 'count': 32 } },
 ]
 
@@ -61,10 +63,23 @@ test_files = [
 def test_profile_assignment(ascii_file):
     setup_testing_logger(level=logging.WARNING)
     ascii = resource('slocum', ascii_file['deployment'], ascii_file['mode'], 'ascii', ascii_file['name'])
+    config = resource('slocum', ascii_file['deployment'], 'config', 'deployment.json')
+    
     df = SlocumReader(ascii).standardize()
 
-    profiles = assign_profiles(df)
+    require_inflection = None
+    tsint = None
+    # If the test deployment includes a config with filter args, load them
+    if Path(config).exists():
+        with open(config, 'r') as config_file:
+            filter_args = json.load(config_file).get('filters', {})
+            require_inflection = filter_args.get('require_inflection')
+            tsint = filter_args.get('tsint')
 
+    # Do the profile assignment
+    profiles = assign_profiles(df, tsint, require_inflection=require_inflection)
+
+    # Assert things are as expected
     if ascii_file['expected']['count'] is None:
         assert profiles is None
         return
